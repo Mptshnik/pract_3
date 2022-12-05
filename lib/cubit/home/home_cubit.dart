d8995065cae5b66c4df23fa553f435d03f95e1dd
Mptshnik <cheburkov123@mail.ru>
2022-12-05 23:53:57 +0300

    cubit

---
+++ lib/cubit/home/home_cubit.dart
@@ -0,0 +1,36 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../theme/theme_cubit.dart';
+
+part 'home_state.dart';
+
+class HomeCubit extends Cubit<HomeState> {
+  HomeCubit() : super(HomeInitial());
+
+  int clickCount = 0;
+  List<String> messages = [];
+  String message = '';
+
+  void handleClick(ThemeMode themeMode) {
+    if (themeMode == ThemeMode.dark) {
+      clickCount += 2;
+    } else {
+      clickCount++;
+    }
+
+    message = "Кликер: $clickCount. Тема: ${themeMode.name}";
+
+    emit(ClickState(clickCount));
+
+    messages.add(message);
+  }
+
+  
+  void themeSwitched(BuildContext context) {
+    messages.add(
+        'Вы поменяли тему на ${context.read<ThemeCubit>().themeMode.name}');
+  }
+}
---
+++ lib/cubit/home/home_state.dart
@@ -0,0 +1,13 @@
+part of 'home_cubit.dart';
+
+@immutable
+abstract class HomeState {}
+
+class HomeInitial extends HomeState {}
+
+class ClickState extends HomeState
+{
+  int clickCount;
+
+  ClickState(this.clickCount);
+}
